7150a221a4241d42e05f1f86357f9a7231d3d4b7
Arash <aelahi4@uic.edu>
2024-11-03 21:03:23 -0700

    Changes to the files

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="micelle",
+    name="MLMicelle",
     version="0.1",
     packages=find_packages(),  # Automatically finds and includes all sub-packages
     install_requires=[          # List dependencies if needed
